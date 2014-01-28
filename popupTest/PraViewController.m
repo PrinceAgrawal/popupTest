9d94f2648e33854b4239180c148e65779cc542a9
Prince Agrawal <achievelimitless@gmail.com>
2014-01-28 16:54:13 +0530

    Mistake correction

--- popupTest/PraViewController.m
+++ popupTest/PraViewController.m
@@ -59,16 +59,8 @@
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    CGPoint point = [[[event allTouches] anyObject] locationInView:self.view];
-    for (UIImageView *sView in self.smileyImages)
-    {
-        if (CGRectContainsPoint([sView frame], point))
-        {
-            self.selectedImage= sView.image;
-            [self.popupView setHidden:YES];
-            break;
-        }
-    }
+    self.selectedImage= self.popupImage.image;
+    [self.popupView setHidden:YES];
 }
 
 
